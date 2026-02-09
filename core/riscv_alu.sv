5e179586e57a2e7a6672409545a62a58fc109148
Dude <dudesvult@yandex.ru>
2026-02-09 22:17:29 +0300

    Fixed typo in ALU template

--- core/riscv_alu.sv
+++ core/riscv_alu.sv
@@ -1,13 +1,13 @@
 `include "riscv_opcodes.sv"
 
 module riscv_alu
-{
+(
     input   [ 3:0] alu_op_i,
     input   [31:0] alu_a_i,
     input   [31:0] alu_b_i,
     output  [31:0] alu_c_o
-};
+);
 
 begin
-    
+
 end
